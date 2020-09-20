a491bffe2647c0fd751e37522660fd26e16c66b9
Fr4nkWong <565123258@qq.com>
2020-09-20 09:49:41 +0800

    featrue: adaboost的sklearn实现

--- adaboost/main.py
+++ adaboost/main.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
 import boosting as bst
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
 # 配置项
@@ -10,7 +12,17 @@ testing_dir = './testing.txt'
 
 
 def main():
-    dataset, labels = bst.load_data(trainning_dir)
+    # sklear实现adaboost
+    dataset, labels = bst.load_data(trainning_dir)  # 训练集
+    testset, test_labels = bst.load_data(trainning_dir) # 测试集
+    bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth = 2), algorithm = "SAMME", n_estimators = 10)
+    bdt.fit(dataset, labels)
+    predictions = bdt.predict(dataset)
+    err_arr = np.mat(np.ones((len(dataset), 1)))
+    print('训练集的错误率:%.3f%%' % float(err_arr[predictions != labels].sum() / len(dataset) * 100))
+    predictions = bdt.predict(testset)
+    err_arr = np.mat(np.ones((len(testset), 1)))
+    print('测试集的错误率:%.3f%%' % float(err_arr[predictions != test_labels].sum() / len(testset) * 100))
 
 
 if __name__ == '__main__':
